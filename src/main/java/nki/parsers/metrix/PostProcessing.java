9cb766ddfca71cebaf4d24b6e13e25148e93731d
Bernd van der Veen <b.vd.veen@nki.nl>
2014-12-08 09:24:26 +0100

    Debugging.

--- src/main/java/nki/parsers/metrix/PostProcessing.java
+++ src/main/java/nki/parsers/metrix/PostProcessing.java
@@ -414,7 +414,9 @@ public class PostProcessing {
     cmd.add("--use-bases-mask");
     cmd.add(dmx.ssBaseMaskMap.get(samplesheet.getAbsolutePath()));
     cmd.add("--force");
-        
+    
+    LoggerWrapper.log.log(Level.FINEST, "Script: {0}\nInput: {1}\nOutput: {2}\nSamplesheet: {3}\nBaseMask: {4}", new Object[]{dmx.getBclToFastQPath(), dmx.getBaseWorkingDir(), dmxBaseOut.toString(), samplesheet.getAbsolutePath(), dmx.ssBaseMaskMap.get(samplesheet.getAbsolutePath())});
+    
     if(dmx.getArguments() != null){
         String[] spl = dmx.getArguments().split(" ");
         cmd.addAll(Arrays.asList(spl));
