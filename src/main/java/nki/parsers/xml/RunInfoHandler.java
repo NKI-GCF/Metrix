1d4aee533754a2533eb2f62d6e30479b69a27af9
Bernd van der Veen <b.vd.veen@nki.nl>
2014-12-11 17:15:09 +0100

    Add legacy conditionals for older runs (RunInfo.xml)

--- src/main/java/nki/parsers/xml/RunInfoHandler.java
+++ src/main/java/nki/parsers/xml/RunInfoHandler.java
@@ -96,9 +96,6 @@ public class RunInfoHandler {
 
     sum.setTotalCycles(totalCycles);
 
-    System.out.println("RunName : " + runID);
-    System.out.println("Number of reads :" + readNodes.getLength());
-    
     if (readNodes.getLength() == 1) {
       sum.setRunType("Single Read");
       sum.setIsIndexed(false);
